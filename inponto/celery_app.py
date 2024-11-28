fed5e5439ed615e92ef071b38485e2984b1ea5cd
fabio.cruz <fabiogjo@hotmail.com>
2024-11-28 20:12:13 -0300

    ajuste time execução

--- inponto/celery_app.py
+++ inponto/celery_app.py
@@ -18,7 +18,7 @@ app.conf.beat_schedule = {
     },
     'executar-18-da-tarde': {
         'task': 'tasks.periodic_task_add_point_inponto',
-        'schedule': crontab(hour=18, minute=54),  # Variação de minutos
+        'schedule': crontab(hour=18, minute=0),  # Variação de minutos
     },
 }
 
