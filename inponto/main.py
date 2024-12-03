b346371305f7ec210c7c02bb3befab0f71fc647f
fabio.cruz <fabiogjo@hotmail.com>
2024-12-03 18:23:25 -0300

    corrindo hash de url para autenticar

--- inponto/main.py
+++ inponto/main.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import re
+
 from loguru import logger
 
 class Inponto():
@@ -7,7 +9,8 @@ class Inponto():
     def __init__(self, **kwargs) -> None:
         logger.info('Init Inponto')
         self.login_url = 'https://inponto.vercel.app/api/login'
-        self.authorization_tk_url = 'https://inponto.vercel.app/_next/data/XxNKmFEE8az4rKgqk9uXo/pt-BR/home.json'
+        self.hash = 'X9nmXeaNKYqKxO64Y5R7c' # ver como deixar dinamico, nao encontrei onde essa variavel é gerada, mas encontrei na pagina.
+        self.authorization_tk_url = f'https://inponto.vercel.app/_next/data/{self.hash}/pt-BR/home.json'
         self.user = kwargs['user']
         self.password = kwargs['password']
         self.payload = {"date":f"{kwargs['data']}","latitude":0,"longitude":0,"userData":{"device":"Desktop","gatewayMac":None,"ip":"","operatingSystem":"Windows 10"}}
@@ -21,8 +24,21 @@ class Inponto():
     def get_authorization_tk(self, cookies):
         logger.info('Capturando token de autorização.')
         data = requests.get(self.authorization_tk_url, cookies=cookies)
+        from IPython import embed;embed()
+
+        if data.status_code == 404:
+            self.get_new_hash(data)
+            data = requests.get(self.authorization_tk_url, cookies=cookies)
+            data.raise_for_status()
+
         resp_json = json.loads(data.content)
         return resp_json['pageProps']['persistData']['company']['fromToken']
+
+    def get_new_hash(self, data):
+        padrao = r'"buildId":"(.*?)"'
+        resultado = re.search(padrao, data.content.decode('utf-8'))
+        self.hash = resultado.group(1) if resultado else None
+        self.authorization_tk_url = f'https://inponto.vercel.app/_next/data/{self.hash}/pt-BR/home.json'
     
     def get_cookies(self):
         logger.info('Realizando login.')
